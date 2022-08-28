f6e3d6d52ce06c37e3af54a5efbe215b37bb3a45
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-08-28 12:42:39 +0400

    Time: 202 ms (5.15%), Space: 43.9 MB (5.01%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortDiag(vector<vector<int>>& mat, int x, int y) {
+        for(int i = 0; i < mat.size(); i++)
+        {
+            vector<int> diag;
+            int curX = x, curY = y;
+            while(curX < mat.size() && curY < mat[0].size())
+                diag.push_back(mat[curX++][curY++]);
+            sort(diag.begin(), diag.end());
+            curX = x, curY = y;
+            for(auto elem : diag)
+                mat[curX++][curY++] = elem;
+        }
+    }
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        for(int i = 0; i < mat.size(); i++) //sort starting from each row
+            sortDiag(mat, i, 0);
+        for(int i = 1; i < mat[0].size(); i++) //sort starting from each column
+            sortDiag(mat, 0, i);
+        return mat;
+    }
+};
