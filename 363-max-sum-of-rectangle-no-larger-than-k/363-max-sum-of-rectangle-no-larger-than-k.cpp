e6d925a50f076a18759c7cfd33c38499425cd1a4
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-08-29 11:54:35 +0400

    Time: 1102 ms (82.68%), Space: 190.9 MB (67.45%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
         int maxSum = INT_MIN;
-        for(int l = 0; l < matrix[0].size(); l++)
+        bool rowGreater = matrix[0].size() <= matrix.size();
+        int m = min(matrix.size(), matrix[0].size());
+        int n = max(matrix.size(), matrix[0].size());
+        for(int l = 0; l < m; l++)
         {
-            vector<int> colSum(matrix.size(), 0);
-            for(int r = l; r < matrix[0].size(); r++)
+            vector<int> colSum(n, 0);
+            for(int r = l; r < m; r++)
             {
-                for(int row = 0; row < matrix.size(); row++)
+                for(int k = 0; k < n; k++)
                 {
-                    colSum[row] += matrix[row][r];
+                    if(rowGreater) colSum[k] += matrix[k][r];
+                    else colSum[k] += matrix[r][k];
                 }
                 set<int> cum;
                 cum.insert(0);
@@ -19,7 +23,10 @@ public:
                     curSum += colSum[i];
                     auto it = cum.lower_bound(curSum - k);
                     if(it != cum.end())
+                    {
                         maxSum = max(maxSum, curSum- *it);
+                        if(maxSum == k) return k;
+                    }
                     cum.insert(curSum);
                 }
             }
