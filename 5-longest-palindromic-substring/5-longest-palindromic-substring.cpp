769d8ccf5e997d3a597f98281273e3b63b9fd782
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-02 00:12:21 +0400

    Time: 310 ms (37.45%), Space: 84.8 MB (28.96%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string output;
+        int m = s.size();
+        vector<vector<bool>> visited(m, vector<bool>(m, false));
+        int maxL = m/2, maxR = m/2;
+        helper(s, m/2, m/2, maxL, maxR, visited);
+        output = s.substr(maxL, maxR - maxL + 1);
+        return output;
+    }
+    
+    void helper(string& s, int l, int r, int& maxL, int& maxR, vector<vector<bool>>& visited)
+    {
+        if(l < 0 || r >= s.size()) return;
+        if(visited[l][r]) return;
+        visited[l][r] = true;
+        if(s[l] == s[r])
+        {
+            if(r - l > maxR - maxL)
+            {
+                maxR = r;
+                maxL = l;
+            }
+            helper(s, l - 1, r + 1, maxL, maxR, visited);
+        }
+        helper(s, l, l + 1, maxL, maxR, visited);
+        helper(s, l - 1, l, maxL, maxR, visited);
+        helper(s, l - 1, l + 1, maxL, maxR, visited);
+        helper(s, r - 1, r, maxL, maxR, visited);
+        helper(s, r, r + 1, maxL, maxR, visited);
+        helper(s, r - 1, r + 1, maxL, maxR, visited);
+    }
+};
