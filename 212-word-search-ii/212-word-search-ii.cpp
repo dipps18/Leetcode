556eee47317c14d18b92c084add2468bcdadd8f6
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-01 16:39:29 +0400

    Time: 600 ms (80.86%), Space: 12.9 MB (86.15%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,7 +1,7 @@
 class Trie{
 public:
-    unordered_map<int, Trie*>root;
-    bool isEnd = false;    
+    Trie* root[26] = {nullptr};
+    string word;
 };
 
 class Solution {
@@ -14,42 +14,27 @@ public:
         {
             for(int j = 0; j < board[0].size(); j++)
             {
-                bool found = false;
-                dfs(res, board, node, i, j, "", found);
+                dfs(res, board, node, i, j);
             }
-
         }
         return res;
     }
-    
-    void dfs(vector<string>& res, vector<vector<char>>& board, Trie* node, int i, int j, string word, bool& found)
+    void dfs(vector<string>& res, vector<vector<char>>& board, Trie* node, int i, int j)
     {
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) return;
-        if(board[i][j] == '*') return;
-        char ch = board[i][j] - 'a';
-        if(node->root.find(ch) == node->root.end()) return;
-        word.push_back(ch + 'a');
-        if(node->root[ch]->isEnd)
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '*') return;
+        int ch = board[i][j] - 'a';
+        if(!node->root[ch]) return;
+        if(!node->root[ch]->word.empty())
         {
-            res.push_back(word);
-            if(node->root[ch]->root.size() == 0)
-            {
-                node->root.erase(ch);
-                return;
-            }
-            else
-                node->root[ch]->isEnd = false;
+            res.push_back(node->root[ch]->word);
+            node->root[ch]->word = "";
         }
         board[i][j] = '*';
-        dfs(res, board, node->root[ch], i + 1, j, word, found);
-        dfs(res, board, node->root[ch], i - 1, j, word, found);
-        dfs(res, board, node->root[ch], i, j + 1, word, found);
-        dfs(res, board, node->root[ch], i, j - 1, word, found);
+        dfs(res, board, node->root[ch], i + 1, j);
+        dfs(res, board, node->root[ch], i - 1, j);
+        dfs(res, board, node->root[ch], i, j + 1);
+        dfs(res, board, node->root[ch], i, j - 1);
         board[i][j] = ch + 'a';
-        if(found && node->root[ch]->root.size() == 0)
-        {
-            node->root.erase(ch);
-        }
     }
     
     void BuildTrie(Trie* root, vector<string> &words)
@@ -64,7 +49,7 @@ public:
                 if(!node->root[ch]) node->root[ch] = new Trie();
                 node = node->root[ch];
             }
-            node->isEnd = true;
+            node->word = words[i];
         }
     }
 };
