52581eb04d204fd7df9c9cb049e92fa2b55cfb46
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-01 18:52:42 +0400

    Time: 52 ms (17.84%), Space: 14.8 MB (23.86%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_set<int> visited;
+        vector<vector<int>> courses(numCourses);
+        
+        for(int i = 0; i < prerequisites.size(); i++)
+        {
+            courses[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        
+        for(int i = 0; i < courses.size(); i++)
+        {
+            if(!helper(visited, courses, i)) return false;
+        }
+        return true;
+    }
+    
+    bool helper(unordered_set<int> &visited, vector<vector<int>>& courses, int course)
+    {
+        
+        if(!visited.insert(course).second) return false;
+        for(int j = 0; j < courses[course].size(); j++)
+        {
+            if(!helper(visited, courses, courses[course][j])) return false;
+        }
+        courses[course].clear();
+        visited.erase(course);
+        return true;
+    }
+};
