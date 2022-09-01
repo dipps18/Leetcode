40608717e2973f307878b161203bbffdbdd6a3ce
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-01 18:58:25 +0400

    Time: 29 ms (71.06%), Space: 14.6 MB (27.34%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -12,7 +12,7 @@ public:
         return true;
     }
     
-    bool helper(unordered_set<int> visited, vector<vector<int>>& courses, int course)
+    bool helper(unordered_set<int>& visited, vector<vector<int>>& courses, int& course)
     {
         if(!visited.insert(course).second) return false;
         for(int j = 0; j < courses[course].size(); j++)
@@ -20,6 +20,7 @@ public:
             if(!helper(visited, courses, courses[course][j])) return false;
         }
         courses[course].clear();
+        visited.erase(course);
         return true;
     }
 };
