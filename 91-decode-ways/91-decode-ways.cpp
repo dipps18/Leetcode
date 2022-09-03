795f98775180d380eaa3a986a088361e1fdf3dfc
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-03 17:44:28 +0400

    Time: 44 ms (5.08%), Space: 6.3 MB (29.26%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -8,10 +8,10 @@ public:
     int helper(string& s, int i, vector<int>&dp)
     {        
         if(i > s.size()) return 0;
-        if(s[i] == '0') return 0;    
         //Indicates that we have passed the last element or are on the last element
         //Eg: 12 -> When i = 1 and when i = 2
-        if(i == s.size() || i == s.size() - 1) return 1;
+        if(i == s.size() || (i == s.size() - 1 && s[i] != '0')) return 1;
+        if(s[i] == '0') return 0;    
         if(dp[i]) return dp[i];
         if(s[i] >= '2')
         {
