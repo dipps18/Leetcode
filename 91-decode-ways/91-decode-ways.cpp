a5aaf9294fe0dc14214df257ed9dd985b7a7d63a
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-03 18:01:46 +0400

    Time: 27 ms (5.08%), Space: 6.3 MB (49.64%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -7,20 +7,14 @@ public:
     
     int helper(string& s, int i, vector<int>&dp)
     {        
-        if(i > s.size()) return 0;
-        //Indicates that we have passed the last element or are on the last element
-        //Eg: 12 -> When i = 1 and when i = 2
-        if(i == s.size() || (i == s.size() - 1 && s[i] != '0')) return 1;
-        if(s[i] == '0') return 0;    
+        if(s[i] == '0') return 0;
+        if(i >= s.size() - 1) return 1;
         if(dp[i]) return dp[i];
-        if(s[i] >= '2')
-        {
-            if((i + 1 < s.size() && s[i + 1] > '6') || s[i] > '2'){
-                dp[i] = helper(s, i + 1, dp);
-                return dp[i];
-            }
-        }
-        dp[i] = helper(s, i + 2, dp) + helper(s, i + 1, dp);
-        return dp[i];
+    
+        int res = helper(s, i + 1, dp);
+        if(i + 1 < s.size() && ((s[i] == '2' && s[i + 1] <= '6') || s[i] == '1'))
+            res += helper(s, i + 2, dp);
+        dp[i] = res;
+        return res;
     }
 };
