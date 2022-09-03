342d5f1ec9f22ba3a5a9efd759522dab4b66759d
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-03 17:23:48 +0400

    Time: 59 ms (5.08%), Space: 6.3 MB (29.26%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> dp(100, 0);
+        return helper(s, 0, dp);
+    }
+    
+    int helper(string& s, int i, vector<int>&dp)
+    {        
+        if(i > s.size()) return 0;
+        if(s[i] == '0') return 0;    
+        //Indicates that we have passed the last element or are on the last element
+        //Eg: 12 -> When i = 1 and when i = 2
+        if(i == s.size() || i == s.size() - 1) return 1;
+        if(dp[i]) return dp[i];
+        if(s[i] >= '2')
+        {
+            if((i + 1 < s.size() && s[i + 1] > '6') || s[i] > '2'){
+                dp[i] = helper(s, i + 1, dp);
+                return dp[i];
+            }
+        }
+        dp[i] = helper(s, i + 2, dp) + helper(s, i + 1, dp);
+        return dp[i];
+    }
+};
