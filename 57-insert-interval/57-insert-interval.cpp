a29bebe9fe46e7c3768dbcd00ba70d1973e1edb5
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-04 23:22:15 +0400

    Time: 29 ms (42.95%), Space: 17.2 MB (38.15%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -2,44 +2,20 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> output;
-        bool pushed = false;
-        if(intervals.empty()) return {newInterval};
-        for(int i = 0; i < intervals.size(); i++)
+        int i = 0, start = newInterval[0], end = newInterval[1];
+        while(i < intervals.size() && intervals[i][1] < newInterval[0])
+            output.push_back(intervals[i++]);
+        
+        while(i < intervals.size() && intervals[i][0] <= newInterval[1])
         {
-            if(newInterval[0] > intervals[i][0] && newInterval[0] > intervals[i][1])
-                output.push_back(intervals[i]);
-            else if(newInterval[0] < intervals[i][0] && newInterval[1] < intervals[i][0])
-            {
-                if(!pushed)
-                {
-                    output.push_back(newInterval);
-                    pushed = true;
-                }
-                output.push_back(intervals[i]);
-            }
-            else
-            {
-                pushed = true;
-                int s = min(intervals[i][0], newInterval[0]);
-                int e = max(intervals[i][1], newInterval[1]);
-                while(i < intervals.size())
-                {
-                    if(intervals[i][0] > e)
-                    {
-                        i--;
-                        break;
-                    }
-                    if(e <= intervals[i][1])
-                    {
-                        e = max(e, intervals[i][1]);
-                        break;
-                    }
-                    i++;
-                }
-                output.push_back({s, e});
-            }
+            start = min(start, intervals[i][0]);
+            end = max(intervals[i++][1], end);   
         }
-        if(!pushed) output.push_back(newInterval);
+        
+        output.push_back({start, end});
+        while(i < intervals.size())
+            output.push_back(intervals[i++]);
+        
         return output;
     }
 };
