e5e60cd73ff53f6ff8406804ea23950276287740
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-07 00:34:28 +0400

    Time: 102 ms (61.54%), Space: 48.3 MB (93.50%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int minJump = nums.size() - 1;
-        for(int i = nums.size() - 2; i >= 0; i--)
+        int n = nums.size();
+        int minJump = n - 1;
+        for(int i = n - 2; i >= 0; i--)
         {
             if(nums[i] + i >= minJump)
                 minJump = i;
