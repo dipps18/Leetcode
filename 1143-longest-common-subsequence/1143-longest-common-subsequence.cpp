07a8b34af322898c2967df6a598b1dd6af2af662
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-08 00:28:51 +0400

    Time: 28 ms (67.88%), Space: 6.3 MB (98.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int longestCommonSubsequence(string t1, string t2) {
-        vector<vector<int>> dp(t1.size(), vector<int>(t2.size(), -1));
-        return helper(t1, t2, dp, 0, 0);
-    }
-    
-    int helper(string& t1, string& t2, vector<vector<int>>& dp, int i, int j)
-    {
-        if(i >= t1.size() || j >= t2.size() || i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(t1[i] == t2[j])
-            dp[i][j] = 1 + helper(t1, t2, dp, i + 1, j + 1);
-        else
-            dp[i][j] = max(helper(t1, t2, dp, i + 1, j), helper(t1, t2, dp, i, j + 1));
-        return dp[i][j];
+        vector<int> cur(t2.size() + 1, 0), prev(t2.size() + 1, 0);
+        for(int i = 1; i <= t1.size(); i++)
+        {
+            for(int j = 1; j <= t2.size(); j++)
+            {
+                if(t1[i - 1] == t2[j - 1])
+                    cur[j] = max(cur[j - 1], prev[j - 1] + 1);
+                else
+                    cur[j] = max(cur[j - 1], prev[j]);
+            }
+            swap(cur, prev);
+        }
+        return prev[t2.size()];
     }
 };
 
