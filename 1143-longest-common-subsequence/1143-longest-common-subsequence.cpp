4b8eb5e601f037e43373f6e732e136427d5b4e15
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-07 23:53:19 +0400

    Time: 94 ms (5.10%), Space: 13.3 MB (11.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int longestCommonSubsequence(string t1, string t2) {
-        vector<vector<int>> dp(t1.size(), vector<int>(t2.size(), 0));
+        vector<vector<int>> dp(t1.size(), vector<int>(t2.size(), -1));
         return helper(t1, t2, dp, 0, 0);
     }
     
     int helper(string& t1, string& t2, vector<vector<int>>& dp, int i, int j)
     {
         if(i >= t1.size() || j >= t2.size() || i < 0 || j < 0) return 0;
-        if(dp[i][j]) return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
         if(t1[i] == t2[j])
             dp[i][j] = 1 + helper(t1, t2, dp, i + 1, j + 1);
         else
