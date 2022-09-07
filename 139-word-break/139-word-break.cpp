27c77c5704a7e4b6bdcd782ae42e3ea2be13d597
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-07 17:50:20 +0400

    Time: 69 ms (9.61%), Space: 7.8 MB (86.56%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,47 +2,24 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int m = s.size();
-        int n = wordDict.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(s, wordDict, dp, 0);
-    }
-    
-    bool helper(string& s, vector<string>& wordDict, vector<vector<int>>& dp, int idx)
-    {
-        if(idx == s.size()) return true;
-        if(idx > s.size()) return false;
-        for(int i = 0; i < wordDict.size(); i++)
+        vector<bool> dp(m + 1, false);
+        dp[m] = true;
+        for(int i = m - 1; i >= 0; i--)
         {
-            if(dp[idx][i] == 0) continue;
-            //If we haven't checked the sequence yet
-            if(dp[idx][i] == -1)
+            string sub;
+            for(int j = i; j < s.size(); j++)
             {
-                if(wordDict[i].size() + idx <= s.size())
+                sub += s[j];
+                for(string word : wordDict)
                 {
-                    int j = idx;
-                    //For checking that the letters match
-                    for(char ch : wordDict[i])
+                    if(word == sub && dp[i + sub.size()]) 
                     {
-                        if(s[j++] != ch) 
-                        {
-                            dp[idx][i] = 0;
-                            break;
-                        }
+                        dp[i] = true;
+                        break;
                     }
-                }
-                else 
-                    dp[idx][i] = 0;
-                
-                // The letters match
-                if(dp[idx][i] != 0)
-                {
-                    if(helper(s, wordDict, dp, idx + wordDict[i].size()) == true)
-                        return true;
-                    else
-                        dp[idx][i] = 0;
                 }
             }
         }
-        return false;
+        return dp[0];
     }
 };
