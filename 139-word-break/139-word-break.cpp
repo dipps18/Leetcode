1db30712b16becd3da0627817f5647df9bf541f9
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-07 17:58:16 +0400

    Time: 0 ms (100.00%), Space: 7.7 MB (89.14%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -2,24 +2,38 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int m = s.size();
-        vector<bool> dp(m + 1, false);
-        dp[m] = true;
-        for(int i = m - 1; i >= 0; i--)
+        vector<int> dp(m, -1);
+        return helper(s, wordDict, dp, 0);
+    }
+    
+    bool helper(string& s, vector<string>& wordDict, vector<int>& dp, int idx)
+    {
+        if(idx == s.size()) return true;
+        if(idx > s.size()) return false;
+        if(dp[idx] != -1) return dp[idx];
+        for(int i = 0; i < wordDict.size(); i++)
         {
-            string sub;
-            for(int j = i; j < s.size(); j++)
+            bool found = true;
+            if(wordDict[i].size() + idx <= s.size())
             {
-                sub += s[j];
-                for(string word : wordDict)
+                int j = idx;
+                //For checking that the letters match
+                for(char ch : wordDict[i])
                 {
-                    if(word == sub && dp[i + sub.size()]) 
+                    if(s[j++] != ch) 
                     {
-                        dp[i] = true;
+                        found = false;
                         break;
                     }
                 }
+                // The letters match
+                if(found)
+                {
+                    if(helper(s, wordDict, dp, idx + wordDict[i].size()) == true)
+                        return dp[idx] = 1;
+                }
             }
         }
-        return dp[0];
+        return dp[idx] = false;
     }
 };
