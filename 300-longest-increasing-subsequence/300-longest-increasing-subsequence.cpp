90ffdcc2e1d2ca209ccc4910b9583bce2c8dd05a
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-07 21:53:43 +0400

    Time: 2462 ms (5.00%), Space: 13.6 MB (17.59%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -5,14 +5,15 @@ public:
         int maxFreq = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            dfs(nums, dp, maxFreq, nums[i], i);
+            if(!dp[i])
+                dfs(nums, dp, maxFreq, nums[i], i);
         }
         return maxFreq;
     }
     
     int dfs(vector<int>& nums, unordered_map<int, int>& dp, int& maxFreq, int val, int idx)
     {
-        if(dp.find(idx) != dp.end()) return dp[idx];
+        if(dp[idx]) return dp[idx];
         dp[idx] = 1;
         for(int i = idx + 1; i < nums.size(); i++)
         {
