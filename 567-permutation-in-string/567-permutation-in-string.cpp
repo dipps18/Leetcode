b027caa2034db6e023ee91e8dee96433dba0a5b4
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-09-13 16:09:50 +0400

    Time: 3 ms (98.63%), Space: 7.2 MB (90.41%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2)
     {
-        vector<int> dict(26, 0), needed(26,0);
+        vector<int> needed(26,0);
         int count = 0;
         int m = s1.size(), n = s2.size(); 
         int left = 0, right = 0;
         for(int i = 0; i < m; i++)
-        {
-            dict[s1[i] - 'a']++;
             needed[s1[i] - 'a']++;
-        }
         
         while(right < n)
         {
@@ -20,11 +17,16 @@ public:
                 right++;
                 count++;
             }
+            else if(count > 0)
+            {
+                count--;
+                needed[s2[left] - 'a']++;
+                left++;
+            }
             else
             {
-                right = ++left;
-                needed = dict;
-                count = 0;
+                right++;
+                left++;
             }
             if(count == m) return true;
         }
