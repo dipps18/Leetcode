7c714899da2adb594ac6643883aa18f4f9626f41
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-01 23:08:33 +0400

    Time: 0 ms (100.00%), Space: 7.6 MB (13.10%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(100, -1);
-        return helper(s, 0, dp);
+        vector<int> dp(101, -1);
+        dp[s.size()] = 1;
+        return dfs(s, 0, dp);
     }
     
-    int helper(string& s, int i, vector<int>&dp)
-    {        
-        if(s[i] == '0') return dp[i] = 0;
-        if(i == s.size()) return 1;
+    int dfs(string s, int i, vector<int>& dp)
+    {
         if(dp[i] != -1) return dp[i];
-    
-        int res = helper(s, i + 1, dp);
-        if(i + 1 < s.size() && ((s[i] == '2' && s[i + 1] <= '6') || s[i] == '1'))
-            res += helper(s, i + 2, dp);
-        dp[i] = res;
-        return res;
+        if(s[i] == '0') return dp[i] = 0;
+        int res = dfs(s, i + 1, dp);
+        if(i + 1 < s.size() && ((s[i] == '2' && s[i + 1] <= '6' && s[i + 1] >= '0') || s[i] == '1'))
+            res += dfs(s, i + 2, dp);
+        return dp[i] = res;
     }
 };
