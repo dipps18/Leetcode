1d811f53d1612e3a6fcc8d05e28ce9772acd7e58
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-02 13:32:58 +0400

    Time: 24 ms (84.00%), Space: 8.4 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
         return dfs(n,k,target,dp);
     }
     
     int dfs(int n, int k, int target, vector<vector<int>>& dp)
     {
-        if(target == 0 && n > 0) return 0;
-        if((target > 0 && n == 0) || target < 0 || target > n * k) return 0;
-        if(target == 0 && n == 0) return 1;
-        if(dp[n][target]) return dp[n][target];
+        if(dp[n][target] != -1) return dp[n][target];
+        if(target == 0 && n > 0) return dp[n][target] = 0;
+        if((target > 0 && n == 0) || target > n * k) return dp[n][target] = 0;
+        if(target == 0 && n == 0) return dp[n][target] = 1;
+        dp[n][target] = 0;
         for(int i = min(target, k); i > 0; i--)
         {
             dp[n][target] = (dp[n][target] + dfs(n - 1, k, target - i, dp)) % 1000000007;
