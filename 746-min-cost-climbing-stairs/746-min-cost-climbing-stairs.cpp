fd957880ce9eaf11c442eaa93760c0e63091d147
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-03 22:47:31 +0400

    Time: 11 ms (60.74%), Space: 13.7 MB (81.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(1001, -1);
-        return min(dfs(cost, 0, dp), dfs(cost, 1, dp));
-    }
-    
-    int dfs(vector<int>& cost, int step, vector<int>& dp)
-    {
-        if(step >= cost.size()) return 0;
-        if(dp[step] != -1) return dp[step];
-        dp[step] = min(dfs(cost, step + 1, dp), dfs(cost, step + 2,dp)) + cost[step];
-        return dp[step];
+        int next = 0, next_two = 0, cur = INT_MAX;
+        for(int i = cost.size() - 1; i >= 0; i--)
+        {
+            cur = min(next, next_two) + cost[i];
+            swap(next_two, next);
+            swap(cur, next);
+        }
+        return min(next, next_two);
     }
 };
