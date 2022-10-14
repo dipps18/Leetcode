6074f61ad5aab80a82cf6fd4e158b69f21b44edf
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-14 22:22:23 +0200

    Time: 35 ms (54.01%), Space: 8.8 MB (24.44%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+string minWindow(string s, string t) {
+    if (t.size() > s.size()) return "";
+    vector<int> dict(128, 0), have(128, 0);
+    queue<int> positions;
+    string res = "";
+    int right = 0, left = 0, count = 0, minSize = INT_MAX, minL = -1, minR = 0;
+    for (char c : t)
+        dict[c - 'A']++;
+    
+    while (right < s.size())
+    {
+        if (dict[s[right] - 'A'])
+        {
+            have[s[right] - 'A']++;
+            if (have[s[right] - 'A'] <= dict[s[right] - 'A'])
+                count++;
+            if (left != right)
+                positions.push(right);
+            while (count == t.size())
+            {
+                if (minSize > right - left + 1)
+                {
+                    minL = left;
+                    minR = right;
+                    minSize = right - left + 1;
+                }
+                if (--have[s[left] - 'A'] < dict[s[left] - 'A'])
+                    count--;
+                if (!positions.empty())
+                {
+                    left = positions.front();
+                    positions.pop();
+                }
+            }
+        }
+        if(!dict[s[left] - 'A']) left++;
+        right++;
+    }
+    
+    if(minL == -1) return "";
+    for (int i = minL; i <= minR; i++)
+        res += s[i];
+    return res;
+}
+};
