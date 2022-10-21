ece61e99a3421599653960001ef0878bb8836641
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-21 17:46:46 +0200

    Time: 41 ms (53.83%), Space: 16.6 MB (80.30%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(100001, -1);
-        dp[nums.size() - 1] = 0;
-        return(dfs(nums, 0, dp));
-    }
-    
-    int dfs(vector<int>& nums, int step, vector<int>& dp)
-    {
-        if(step >= nums.size()) return 0;
-        if(dp[step] != -1) return dp[step];
-        dp[step] = INT_MAX - 1;
-        for(int i = 1; i <= nums[step]; i++)
+        int curEnd = 0, curFarthest = 0, jumps = 0;
+        for(int i = 0; i < nums.size() - 1; i++)
         {
-            dp[step] = min(dp[step], dfs(nums, step + i, dp) + 1);
-            if(dp[step] == 1) return dp[step];
+            curFarthest = max(curFarthest, i + nums[i]);
+            if(i == curEnd)
+            {
+                jumps++;
+                curEnd = curFarthest;
+            }
+            if(curEnd == nums.size() - 1) break;
         }
-        return dp[step];        
+        return jumps;
     }
 };
