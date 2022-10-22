b361c1ebb9b819b21132f3042760ddc229aa4e5d
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2022-10-22 22:30:17 +0200

    Time: 91 ms (26.32%), Space: 19.1 MB (17.72%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        sort(coins.begin(), coins.end());
         vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
         return dfs(amount, 0, coins, dp);
     }
@@ -8,11 +9,12 @@ public:
     int dfs(int amountLeft, int index, vector<int>& coins, vector<vector<int>>& dp)
     {
         if(amountLeft == 0)
-            return 1;
+            return dp[index][amountLeft] = 1;
         if(amountLeft < 0 || index == coins.size()) 
             return 0;
         if(dp[index][amountLeft] != -1) 
             return dp[index][amountLeft];
+        
         dp[index][amountLeft] = dfs(amountLeft - coins[index], index, coins, dp) +                                           dfs(amountLeft, index + 1, coins, dp);
         return dp[index][amountLeft];
     }
