b20dc6f2fd0e9e880d802d617946fae1516c2a35
Dipankar Purecha <61528434+dipps18@users.noreply.github.com>
2023-10-01 11:43:14 +0200

    Time: 95 ms (47.59%), Space: 27.1 MB (39.03%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -5,20 +5,11 @@ public:
         
         int l = 1, r = 1;
         int currentCount = 1, maxCount = 0;
-        char vowels[5] = {'a','e','i','o','u'};
-        char succeeding[128];
         int differentCharCount = 1;
-        
-        for(int i = 0; i < 4; i++)
-        {
-            succeeding[vowels[i]] = vowels[i + 1];
-        }
-        
-        succeeding['u'] = 'u';
         
         while(r < word.length())
         {
-            if(word[r] == word[r - 1] || word[r] == succeeding[word[r - 1]])
+            if(word[r] >= word[r - 1])
             {
                 if(word[r] != word[r - 1])
                     differentCharCount++;
